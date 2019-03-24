30c9585cba4011f50622698d8993a7e0f5fd5e18
leerock <1062498@qq.com>
2019-03-24 17:18:33 +0800

    the project to combine all dependencies

---
+++ src/main/java/hpack/DH.java
@@ -0,0 +1,15 @@
+package hpack;
+
+import org.joda.time.DateTimeUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class DH
+{
+    public static void showCurrentTime(String input)
+    {
+        System.out.println( input + ": " + DateTimeUtils.currentTimeMillis());
+    }
+}
---
+++ src/main/java/hpack/DH1.java
@@ -0,0 +1,15 @@
+package hpack;
+
+import org.joda.time.DateTimeUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class DH1
+{
+    public static void showCurrentTime(String input)
+    {
+        System.out.println( input + ": " + DateTimeUtils.currentTimeMillis());
+    }
+}
